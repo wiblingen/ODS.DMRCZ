3056fc09c932415991c691aec9d4091367e5428e
wiblingen <105824369+wiblingen@users.noreply.github.com>
2026-01-04 15:35:46 +0100

    Add files via upload

--- config/version.php
+++ config/version.php
@@ -7,10 +7,10 @@ $configUpdateRequired = 2023010400;
 
 $gitBranch = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git branch | grep '*' | cut -f2 -d ' '");
 $versionCmd = exec("git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git rev-parse --short=10 $gitBranch");
-$verNo = "<span title='WPSD Version # $versionCmd'>WPSD Ver.# $versionCmd</span>";
+$verNo = "<span title='WPSD Version # $versionCmd'>ODS Ver.# $versionCmd</span>";
 
 if ($gitBranch !== "master") {
-    $version = "$verNo / Branch: $gitBranch";
+    $version = "$verNo / Sestava: $gitBranch";
 } else {
     $version = "$verNo";
 }
